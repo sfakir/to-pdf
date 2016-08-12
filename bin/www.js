141da3108f04bde919f7bf5dcac816bd5c424f1e
Rhett Lowe <rng2ml@gmail.com>
2016-08-12 16:34:15 -0700

    adding cores headers

--- bin/www.js
+++ bin/www.js
@@ -38,7 +38,7 @@ var server = restify.createServer({
 server.use(
   function crossOrigin (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     return next();
   }
 );
